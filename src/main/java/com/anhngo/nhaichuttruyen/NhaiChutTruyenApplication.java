f41c57d05fc2eaa2e92ecbcba885400cc44db1b4
Anh Anh <ngovietanh121@gmail.com>
2025-02-16 15:58:15 +0700

    'first'

---
+++ src/main/java/com/anhngo/nhaichuttruyen/NhaiChutTruyenApplication.java
@@ -0,0 +1,13 @@
+package com.anhngo.nhaichuttruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhaiChutTruyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhaiChutTruyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anhngo/nhaichuttruyen/NhaiChutTruyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhngo.nhaichuttruyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhaiChutTruyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
